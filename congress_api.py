bb9e66fba222844137209294b55b7600342f9fc9
MikeBoris <mikeboris123@gmail.com>
2018-02-26 22:05:18 -0500

    cleaned up some funcs

--- congress_api.py
+++ congress_api.py
@@ -6,19 +6,25 @@ congress	105-115
 chamber	house or senate
 type	introduced, updated, passed or major
 """
-
+from collections import Counter
 import json
 import requests
+from textblob import TextBlob
 from ApiKey import params
 
-def proAPI():
-	key = params['api_key']
+def get_house_bills(key='api_key'):
+	"""
+	Given API key, sends get request to propublica's Congress API
+	Returns results as json
+	"""
+	key = params[key]
 	#url = "https://api.propublica.org/congress/v1/115/senate/members.json"
 	# get recent bills
 	url = "https://api.propublica.org/congress/v1/115/house/bills/introduced.json"
 	r = requests.get(url,headers={"X-API-KEY":key})
-	return r.content
-	#return r.content
+	return json.loads(r.content)
+
+'''
 
 def print_data(data):
 	r = json.loads(data)
@@ -29,25 +35,15 @@ def print_data(data):
 		print(bills[i]['title'])
 		print(bills[i]['sponsor_party'])
 		print("")
-
-
-
 '''
 
-construct tables (df) for the top pos:
-	topAdj:
-	ID, adj1, adj2, adj3
-
-	topNoun:
-	ID, noun1, noun2, noun3
-
-	topVerb:
-	ID, verb1, verb2, verb3
-
-
-'''
-from textblob import textblob
-from collections import Counter
+def get_bill_title(json):
+	"""
+	Given json return object
+	Returns list of bill titles
+	"""
+	bills = json['results'][0]['bills']
+	return [bills[i]['title'] for i in range(len(bills))]
 
 #--- some functions for extracting text metrics ----------------
 
@@ -87,17 +83,33 @@ def get_verb(blob):
 	if blob.tags[i][1].startswith('VB')]
 	verbs = Counter(verb)
 	return verbs
-	
 
 
+def get_house_words():
+	json = get_house_bills()
+	title_list = get_bill_title(json)
+	for i in title_list:
+		blob = get_blob(i)
+		a = get_adj(blob)
+		n = get_noun(blob)
+		v = get_verb(blob)
+		print(a, n, v)
 
-	
-		adjectives
-		nouns
-		verbs
+
+'''
+construct tables (df) for the top pos:
+	topAdj:
+	ID, adj1, adj2, adj3
+
+	topNoun:
+	ID, noun1, noun2, noun3
+
+	topVerb:
+	ID, verb1, verb2, verb3
 
 
+'''
 
 if __name__ == '__main__':
-	data = proAPI()
-	print_data(data)
+	
+	get_house_words()
